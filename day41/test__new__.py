12ff30dfdc4501b2b01120f06228f129d6073b94
fixdq <fixd.quan@aliyun.com>
2018-05-15 00:11:40 +0800

    modify

---
+++ day41/test__new__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  : Fixdq
+# @File    : test__new__.py
+# @Software: PyCharm
+
---
+++ day41/元类复习.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  : Fixdq
+# @File    : 元类复习.py
+# @Software: PyCharm
+
