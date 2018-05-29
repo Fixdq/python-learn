23432ecb404532242e5084606ad5931b16a04226
fixdq <fixd.quan@aliyun.com>
2018-05-29 19:49:30 +0800

    modify

---
+++ weekday0602/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 18-5-29 上午8:38
+# @Author  : fixdq
+# @File    : test.py
+# @Software: PyCharm
+
+def extend_list(v, li=[]):
+    li.append(v)
+    return li
+
+list1 = extend_list(10)
+list2 = extend_list(123,[])
+list3 = extend_list('a')
+
+print(list1)
+print(list2)
+print(list3)
+
+print(list1 is list3)
+
+list11 = ["a", "b", "c", "d", "e"]
+print(list11[10:])
